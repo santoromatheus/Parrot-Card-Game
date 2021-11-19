fd1c9f7a6d2f58f96c9a600a4caeea937c20da44
Matheus Santoro <zen.santoro@gmail.com>
2021-11-19 04:04:31 -0300

    Requisito: Clique na Carta

--- script/script.js
+++ script/script.js
@@ -14,7 +14,7 @@ while ( (qntCartas%2 !== 0) || (qntCartas < 2) || (qntCartas > 14) ) {
 inserir as cartas viradas pra baixo na página 
 de forma que a distribuição seja aleatória */
 
-const cartasPossiveis = [`<div class="carta"><img src="imagens/front.png" alt="front"/><img class="sumir" src="./imagens/bobrossparrot.gif" alt="bobrossparrot"/></div>`, `<div class="carta"><img src="imagens/front.png" alt="front"/><img class="sumir" src="./imagens/explodyparrot.gif" alt="explodyparrot"/></div>`, `<div class="carta"><img src="imagens/front.png" alt="front"/><img class="sumir" src="./imagens/fiestaparrot.gif" alt="fiestaparrot"/></div>`, `<div class="carta"><img src="imagens/front.png" alt="front"/><img class="sumir" src="./imagens/fiestaparrot.gif" alt="fiestaparrot"/></div>`, `<div class="carta"><img src="imagens/front.png" alt="front"/><img class="sumir" src="./imagens/revertitparrot.gif" alt="revertitparrot"/></div>`, `<div class="carta"><img src="imagens/front.png" alt="front"/><img class="sumir" src="./imagens/tripletsparrot.gif" alt="tripletsparrot"/></div>`, `<div class="carta"><img src="imagens/front.png" alt="front"/><img class="sumir" src="./imagens/unicornparrot.gif" alt="unicornparrot"/></div>`];
+const cartasPossiveis = [`<div class="carta" onclick="virarCarta(this)"> <img class="paraBaixo" src="imagens/front.png" id="front"/> <img class="paraCima sumir" src="./imagens/bobrossparrot.gif" id="bobrossparrot"/> </div>`, `<div class="carta" onclick="virarCarta(this)"><img class="paraBaixo" src="imagens/front.png" id="front"/><img class="paraCima sumir" src="./imagens/explodyparrot.gif" id="explodyparrot"/></div>`, `<div class="carta" onclick="virarCarta(this)"><img class="paraBaixo" src="imagens/front.png" id="front"/><img class="paraCima sumir" src="./imagens/fiestaparrot.gif" id="fiestaparrot"/></div>`, `<div class="carta" onclick="virarCarta(this)"><img class="paraBaixo" src="imagens/front.png" id="front"/><img class="paraCima sumir" src="./imagens/metalparrot.gif" id="metalparrot"/></div>`, `<div class="carta" onclick="virarCarta(this)"><img class="paraBaixo" src="imagens/front.png" id="front"/><img class="paraCima sumir" src="./imagens/revertitparrot.gif" id="revertitparrot"/></div>`, `<div class="carta" onclick="virarCarta(this)"><img class="paraBaixo" src="imagens/front.png" id="front"/><img class="paraCima sumir" src="./imagens/tripletsparrot.gif" id="tripletsparrot"/></div>`, `<div class="carta" onclick="virarCarta(this)"><img class="paraBaixo" src="imagens/front.png" id="front"/><img class="paraCima sumir" src="./imagens/unicornparrot.gif" id="unicornparrot"/></div>`];
 let cartasNoJogo = [];
 
 for(let i = 0; i < qntCartas/2; i++) {
@@ -30,14 +30,35 @@ for(let i = 0; i < qntCartas; i++) {
     tabuleiro.innerHTML += cartasNoJogo[i];
 }
 
-
-
-
 function comparador() {
     return Math.random() - 0.5;
 }
 
+function virarCarta(element) {
+    if( (element.classList.contains("first") === false) && (element.classList.contains("matched") === false)) {
+        element.querySelector(`.paraBaixo`).classList.toggle("sumir");
+        element.querySelector(`.paraCima`).classList.toggle("sumir");
+        if(document.querySelector(`.first`) === null) {
+            element.classList.add("first");
+        } else if (document.querySelector(`.first`).querySelector(`.paraCima`).id === element.querySelector(`.paraCima`).id ){
+            document.querySelector(`.first`).classList.add("matched");
+            document.querySelector(`.first`).classList.remove("first");
+            element.classList.add("matched");
+        } else {
+            element.classList.add("unmatched");
+            setTimeout(voltarParaBaixo, 1000);
+        }
+    }
+}
 
+function voltarParaBaixo() {
+    document.querySelector(`.unmatched`).querySelector(`.paraBaixo`).classList.toggle("sumir");
+    document.querySelector(`.unmatched`).querySelector(`.paraCima`).classList.toggle("sumir");
+    document.querySelector(`.unmatched`).classList.remove("unmatched");
+    document.querySelector(`.first`).querySelector(`.paraBaixo`).classList.toggle("sumir");
+    document.querySelector(`.first`).querySelector(`.paraCima`).classList.toggle("sumir");
+    document.querySelector(`.first`).classList.remove("first");
+}
 
 
 
