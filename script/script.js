238d0a12350ac108aed07754263577a8b752c6e7
Matheus Santoro <zen.santoro@gmail.com>
2021-11-19 01:24:27 -0300

    ajustes no Requisito anterior

--- script/script.js
+++ script/script.js
@@ -13,15 +13,32 @@ while ( (qntCartas%2 !== 0) || (qntCartas < 2) || (qntCartas > 14) ) {
 /* Após inserir um número de cartas válido, o jogo deverá 
 inserir as cartas viradas pra baixo na página 
 de forma que a distribuição seja aleatória */
-const imgCartaParaCima = [`<img src="./imagens/bobrossparrot.gif" alt="bobrossparrot"/>`, `<img src="./imagens/explodyparrot.gif" alt="explodyparrot"/>`, `<img src="./imagens/fiestaparrot.gif" alt="fiestaparrot"/>`, `<img src="./imagens/fiestaparrot.gif" alt="fiestaparrot"/>`, `<img src="./imagens/revertitparrot.gif" alt="revertitparrot"/>`, `<img src="./imagens/tripletsparrot.gif" alt="tripletsparrot"/>`, `<img src="./imagens/unicornparrot.gif" alt="unicornparrot"/>`];
-const imgCartaParaBaixo = `<div class="carta"><img src="imagens/front.png" alt="front"/></div>`;
 
-const linhas = document.querySelectorAll(".linha");
+const cartasPossiveis = [`<div class="carta"><img src="imagens/front.png" alt="front"/><img class="sumir" src="./imagens/bobrossparrot.gif" alt="bobrossparrot"/></div>`, `<div class="carta"><img src="imagens/front.png" alt="front"/><img class="sumir" src="./imagens/explodyparrot.gif" alt="explodyparrot"/></div>`, `<div class="carta"><img src="imagens/front.png" alt="front"/><img class="sumir" src="./imagens/fiestaparrot.gif" alt="fiestaparrot"/></div>`, `<div class="carta"><img src="imagens/front.png" alt="front"/><img class="sumir" src="./imagens/fiestaparrot.gif" alt="fiestaparrot"/></div>`, `<div class="carta"><img src="imagens/front.png" alt="front"/><img class="sumir" src="./imagens/revertitparrot.gif" alt="revertitparrot"/></div>`, `<div class="carta"><img src="imagens/front.png" alt="front"/><img class="sumir" src="./imagens/tripletsparrot.gif" alt="tripletsparrot"/></div>`, `<div class="carta"><img src="imagens/front.png" alt="front"/><img class="sumir" src="./imagens/unicornparrot.gif" alt="unicornparrot"/></div>`];
+let cartasNoJogo = [];
 
 for(let i = 0; i < qntCartas/2; i++) {
-    linhas[0].innerHTML += imgCartaParaBaixo;
-    linhas[1].innerHTML += imgCartaParaBaixo;
+    cartasNoJogo.push(cartasPossiveis[i]);
+    cartasNoJogo.push(cartasPossiveis[i]);
 }
+
+cartasNoJogo.sort(comparador);
+
+const tabuleiro = document.querySelector("main");
+
+for(let i = 0; i < qntCartas; i++) {
+    tabuleiro.innerHTML += cartasNoJogo[i];
+}
+
+
+
+
+function comparador() {
+    return Math.random() - 0.5;
+}
+
+
+
 
 
 
